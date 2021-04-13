3e6c4d662e3b66efcbf25eb972a033d53b3f9a40
ArmCode22 <81496048+ArmCode22@users.noreply.github.com>
2021-04-13 13:50:33 -0400

    S

--- Palindrome Series/palindrome.java
+++ Palindrome Series/palindrome.java
@@ -9,25 +9,33 @@ class palindrome {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("palindrome.out")));
         
 
+        
         for(int i=0; i<numCases; i++){
             ArrayList<Integer> failed = new ArrayList<Integer>();
             int numwords = Integer.parseInt(br.readLine());
+
             for(int j=0; j<numwords; j++){
+                //load in data and make everything lowercase
                 String word = br.readLine();
                 char[] chars = word.toCharArray();
                 for(int k=0; k<chars.length; k++){
                     chars[k] = Character.toLowerCase(chars[k]);
                 }
+
+                //check if it is a palindrome
                 boolean palindrome = true;
                     for (int k = 0; k < chars.length/2; k++){
                         if(chars[k]!=chars[chars.length-k-1]){
                             palindrome = false;
                         }
                     }
+                    //if not write which case it is that failed
                     if(!palindrome){
                         failed.add(j+1);
                     }
             }
+
+            //print results
             if(failed.size()==0){
                 pw.println("True");
             }
