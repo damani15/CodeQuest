02c417a1636621910036a3c1a8e927f2404474d8
ArmCode22 <81496048+ArmCode22@users.noreply.github.com>
2021-04-13 12:58:46 -0400

    Suffffix

---
+++ sufFIX/sufFIX.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+import java.io.*;
+
+class palindrome {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("sufFIX.in"));
+        int numCases = Integer.parseInt(br.readLine());
+        
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("sufFIX.out")));
+        
+
+        for(int i=0; i<numCases; i++){
+            String hold = br.readLine();
+            char[] chars = hold.toCharArray();
+        
+            if(chars[chars.length-3] == '1'){
+                
+                chars[chars.length-1] = 't';
+                chars[chars.length-2] = 's';
+               
+                if(chars.length>3){
+                    if(chars[chars.length-4] == '1'){
+                        pw.println(hold);
+                    }
+                    else{
+                        for(int k=0; k<chars.length-1; k++){
+                            pw.print(chars[k]);
+                        }
+                        pw.println(chars[chars.length-1]);
+                    }
+                }
+                else{
+                    for(int k=0; k<chars.length-1; k++){
+                        pw.print(chars[k]);
+                    }
+                    pw.println(chars[chars.length-1]);
+                }
+                
+            }
+            else if(chars[chars.length-3] == '2'){
+                chars[chars.length-1] = 'd';
+                chars[chars.length-2] = 'n';
+                  
+                if(chars.length>3){
+                    if(chars[chars.length-4] == '1'){
+                        pw.println(hold);
+                    }
+                    else{
+                        for(int k=0; k<chars.length-1; k++){
+                            pw.print(chars[k]);
+                        }
+                        pw.println(chars[chars.length-1]);
+                    }
+                }
+                else{
+                    for(int k=0; k<chars.length-1; k++){
+                        pw.print(chars[k]);
+                    }
+                    pw.println(chars[chars.length-1]);
+                }
+            }
+
+            else if(chars[chars.length-3] == '3'){
+                chars[chars.length-1] = 'd';
+                chars[chars.length-2] = 'r';
+                  
+                if(chars.length>3){
+                    if(chars[chars.length-4] == '1'){
+                        pw.println(hold);
+                    }
+                    else{
+                        for(int k=0; k<chars.length-1; k++){
+                            pw.print(chars[k]);
+                        }
+                        pw.println(chars[chars.length-1]);
+                    }
+                }
+                else{
+                    for(int k=0; k<chars.length-1; k++){
+                        pw.print(chars[k]);
+                    }
+                    pw.println(chars[chars.length-1]);
+                }
+            }
+
+            else{
+                pw.println(hold);
+            }
+
+        }
+        pw.close();
+        br.close();
+    }
+}
