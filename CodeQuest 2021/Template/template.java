5e2195f95cc4d43c4bd1d6bb54582255cc75bdfa
alopezforastier22 <81666662+alopezforastier22@users.noreply.github.com>
2021-04-24 07:00:00 -0400

    codequest 2021

---
+++ CodeQuest 2021/Template/template.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class template {
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+            int testCases = Integer.parseInt(input.nextLine());
+
+            for (int testcase = 0; testcase < testCases; testcase++) {
+                StringTokenizer st = new StringTokenizer(input.nextLine(), "");
+            }
+        }
+    }
+}
